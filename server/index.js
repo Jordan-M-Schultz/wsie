38b2cdee3492d61a381e2c0f36a2265377e1f9b1
Jordan Schultz <jmschultz@ucdavis.edu>
2019-07-08 16:53:46 -0700

    added yelp api key and id from env

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+// const request = require('request');
 
 const app = express();
+const YELP_API_KEY = process.env.YELP_API_KEY;
+const YELP_CLIENT_ID = process.env.YELP_CLIENT_ID;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
@@ -11,4 +14,5 @@ app.use(require('./routes'));
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
+  // console.log(process.env.YELP_API_KEY)
 );
