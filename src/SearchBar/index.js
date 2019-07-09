c85a85b38478a7800d8356a4ce69abd2a33cdde0
Jordan Schultz <jmschultz@ucdavis.edu>
2019-07-09 16:39:31 -0700

    updated handleChange and Submit, render, added props from parent

--- src/SearchBar/index.js
+++ src/SearchBar/index.js
@@ -6,33 +6,48 @@ class SearchBar extends Component{
     constructor(props){
         super(props);
         this.state = {
-            searchText:  '',
-            returnedText: '',
-            returnObj: {}
+            restaurantType:  '',
+            location: '',
+            error: false
         }
-        this.handleChange = this.handleChange.bind(this); //this.handleChange is bound to this to call later
-        this.handleSubmit = this.handleSubmit.bind(this);
+        // this.handleChange = this.handleChange.bind(this); //this.handleChange is bound to this to call later
     }
-    handleChange(event){
-        this.setState({searchText: event.target.value});
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value});
     }
-    handleSubmit(event){
-        fetch(`/search?zip=${this.state.searchText}`)
-        .then(response => response.json())
-        // .then(data => this.setState({returnObj:data}));
-        .then(data => console.log(data));
+    handleSubmit = (event) => {
+        if(!this.state.restaurantType || !this.state.location){
+            this.setState({error:true});
+        }else{
+            fetch(`/search?term=${this.state.restaurantType}&location=${this.state.location}`)
+            .then(response => response.json())
+            .then(data => this.props.handleResult(data));
+        }
         event.preventDefault();
     }
+    
 
     render(){
         return(
             <div className = 'SearchBar'>
-                <form className='SearchBar' onSubmit={this.handleSubmit}>
+                <form className='SearchBar input-group' onSubmit={this.handleSubmit}>
                     <button><i className="fas fa-search"></i></button>
                     <input 
-                        type="text" className="form-control " placeholder="Search..." 
-                        value = {this.state.searchText} onChange={this.handleChange}
-                    />    
+                        name = "restaurantType" 
+                        type="text" 
+                        className= {`form-control ${this.state.error ? 'error' : ''}`}
+                        placeholder="Find ..." 
+                        value = {this.state.restaurantType} 
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        name = "location" 
+                        type="text" 
+                        className={`form-control ${this.state.error ? 'error' : ''}`}
+                        placeholder="Near ..." 
+                        value = {this.state.location} 
+                        onChange={this.handleChange}
+                    />
                 </form>
             </div>
         );
