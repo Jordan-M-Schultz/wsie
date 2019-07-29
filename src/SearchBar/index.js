329cbc9bc67ea0e1abcacd988c730d130d8f4d4b
Jordan Schultz <jmschultz@ucdavis.edu>
2019-07-29 16:08:58 -0700

    handle submit error routing

--- src/SearchBar/index.js
+++ src/SearchBar/index.js
@@ -11,6 +11,7 @@ class SearchBar extends Component{
             price: ["","","",""],
             openNow: false,
             error: false,
+            hotNew: false
         }
         // this.handleChange = this.handleChange.bind(this); //this.handleChange is bound to this to call later
     }
@@ -18,21 +19,26 @@ class SearchBar extends Component{
         this.setState({[event.target.name]: event.target.value});
     }
 
+    // Update state based on filter options
     handleClick = (event, pos="") => {
         if(pos !== ""){ //adjusting for price
             let priceCP = [...this.state.price]; // create the copy of state array
+            
             if(priceCP[pos-1] === "") //remember, position index starts at 1, have to shift left
                 priceCP[pos-1] = pos;
             else
                 priceCP[pos-1] = "";
             this.setState({price: priceCP}); 
         }else{
-            this.setState({[event.target.name] : event.target.value});
-            console.log(this.state.openNow);
+            // console.log(event.target.checked);
+            this.setState({[event.target.name] : event.target.checked});
+            console.log(this.state.hotNew);
         }
         
     }
 
+
+    
 
     handleSubmit = (event) => {
         if(!this.state.restaurantType || !this.state.location){
@@ -40,24 +46,59 @@ class SearchBar extends Component{
         }else{
             var fetchURI = `/search?term=${this.state.restaurantType}&location=${this.state.location}`;
             var priceURI = this.state.price.filter(Boolean);
-            if(priceURI)
+            
+            
+            if(priceURI.length !== 0)
                 fetchURI += `&price=${priceURI}`;
+            if(this.state.openNow)
+                fetchURI += `&open_now=${this.state.openNow}`;
+            if(this.state.hotNew)
+                fetchURI += `&attributes=hot_and_new`
+
             console.log('sending', fetchURI);
+
+            //handle HTTP errors here
+            const handleErrors = (response) => {
+                if(!response.ok)
+                    throw Error(response.statusText);
+                return response.json();
+            }
             
             fetch(fetchURI)
-            .then(response => response.json()) //result is response, convert to json
+            .then(handleErrors)
             .then(data => { //data is json, 
                 console.log('passing in', data);
                 this.props.handleRestaurantData(data);
+            }).catch(err => {
+                console.log(err);
+                this.props.handleRestaurantData(null);
+                //output false page here prob
             });
+
+            // fetch(fetchURI)
+            // .then(response => {
+            //     if(!response.ok){
+            //         console.log("Error");
+            //         throw Error(response.statusText);
+            //     }
+            //     return response.json()
+            // }) //result is response, convert to json
+            // .then(data => { //data is json, 
+            //     console.log('passing in', data);
+            //     if(data)
+            //         this.props.handleRestaurantData(data);
+            // }).catch(err => console.log(err));
         }
         
         event.preventDefault();
     }
+
+    
     
 
     render(){
         return(
+            
             <div className = 'SearchBar'>
                 <form className='SearchBar input-group' onSubmit={this.handleSubmit}>
                     <button className = 'filterBtn' type="button" aria-expanded="false" data-toggle="collapse" data-target="#filter"><i className="fas fa-filter"></i></button>
@@ -80,6 +121,8 @@ class SearchBar extends Component{
                     />
 
                 </form>
+                
+                {/* Filter options, check boxes */}
                 
                 <div id = 'filter' className = 'collapse form-group'>
                     <div className="form-check">
