8aaf7fdffea35026205f624e8406026a716a6fee
Jordan Schultz <jmschultz@ucdavis.edu>
2019-07-29 16:07:19 -0700

    added openNow argument

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+require('dotenv').config(); //auto load env information 
+
 //Middle ware that is specific to this router
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
@@ -14,12 +16,19 @@ router.get('/search', (req,res)=>{
     // res.setHeader('Content-Type', 'application/json');
     const type = req.query.term;
     const location = req.query.location;
-    const price = req.query.price
+    const price = req.query.price;
+    const openNow = req.query.open_now;
+    const attributes = req.query.attributes;
+
     var reqURL = `https://api.yelp.com/v3/businesses/search?location=${location}&term=${type}`
     
     if(price)
         reqURL += `&price=${price}`
-    
+    if(openNow)
+        reqURL += `&open_now=${openNow}`
+    if(attributes)
+        reqURL += `&attributes=${attributes}`
+
     const options = {
         url : reqURL,
         headers : {
@@ -32,10 +41,11 @@ router.get('/search', (req,res)=>{
             let min = 0;
             let max = info.businesses.length;
             const random = Math.floor(Math.random()*(max-min+1)+min);
-            // for(var i = 0 ; i<info.businesses.length; i++)
-            //     console.log(info.businesses[i].price);
             res.send(JSON.stringify(info.businesses[random]));
-            // res.send(JSON.stringify(info.businesses));
+        }else{
+            //current catchall for errors
+            res.statusCode = 400;
+            res.send('Error, bad request');
         }
     }
 
