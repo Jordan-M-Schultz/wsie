7ff1973aa62bd239d9f4982289fcce96e5b3fcbd
Jordan Schultz <jmschultz@ucdavis.edu>
2019-07-29 16:07:52 -0700

    added openNow + query updates

--- src/App.js
+++ src/App.js
@@ -9,54 +9,55 @@ class App extends Component {
     super(props); //class constructors must call super in ES6 if they are a subclass
     this.state = {
       name: '',
-      greeting: '',
+      displayError: false,
       restaurant: {}
     };
   }
 
   handleRestaurantData = (data) => {
-    console.log('inside handle restaurant');
-    this.setState({restaurant:data});
-  } 
-    // let min = 0;
-    // let max = data.length;
-    // const random = Math.floor(Math.random()*(max-min+1)+min);
-    // console.log(data);
-    // this.setState({restaurant:data});
-   
-  
-
-
-  render() {
-    if(Object.entries(this.state.restaurant).length === 0 && this.state.restaurant.constructor === Object){
-      return(
-        <div className='App'>
-          <div className='header'>
-            <h1>What Should I Eat?</h1>
-          </div>
-          <div className='wrap-center'>
-            <SearchBar handleRestaurantData={this.handleRestaurantData}/>
-          </div>
-        </div>
-      );
+    if(data){
+      this.setState({restaurant:data});
+      this.setState({displayError: false});
     }else{
-      // const displayAddress = (this.state.restaurant.location.display_address)
-      //   .map((field, index) => <h5 key={index}>{field}</h5> );
-      console.log('first');
-      return(
-        <div className='Content-wrapper'>
-          <div className='header'>
-          </div>
-          <div className='wrap-center-top'>
+      this.setState({displayError: true});
+    }
+  } 
+  
+  render() {
+    if(this.state.displayError){
+      console.log('implement'); 
+    }else{
+      if(Object.entries(this.state.restaurant).length === 0 && this.state.restaurant.constructor === Object){
+        return(
+          <div className='App'>
+            <div className='header'>
+              <h1>What Should I Eat?</h1>
+            </div>
+            <div className='wrap-center'>
               <SearchBar handleRestaurantData={this.handleRestaurantData}/>
-              {/* <h5>WSIE?</h5> */}
+            </div>
           </div>
-          <div className='wrap-center-entry'>
-            <Entry restaurantData={this.state.restaurant}/>
+        );
+      }else{
+        // const displayAddress = (this.state.restaurant.location.display_address)
+        //   .map((field, index) => <h5 key={index}>{field}</h5> );
+        console.log('first');
+        return(
+          <div className='Content-wrapper'>
+            <div className='header'>
+            </div>
+            <div className='wrap-center-top'>
+                <SearchBar handleRestaurantData={this.handleRestaurantData}/>
+                {/* <h5>WSIE?</h5> */}
+            </div>
+            <div className='wrap-center-entry'>
+              <Entry restaurantData={this.state.restaurant}/>
+            </div>
           </div>
-        </div>
-      );
+        );
+      }
     }
   }
+     
 }
 export default App;
